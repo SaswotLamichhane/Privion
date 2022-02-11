ea621ee0bb2bb33dfb27696c2535f8f70cac81a7
Shaswot Lamichhane <saswot.dev@gmail.com>
2022-02-11 19:20:58 +0545

    Improved covid responce

--- alarm.py
+++ alarm.py
@@ -2,7 +2,7 @@ import os,datetime as E,time
 from covidon import covid_return as F
 J='play "/home/shal/Documents/code/Privion/co.mp3"'
 I='covid'
-G=['10:05',0, '11:05',1,'12:05',2,'13:35',3,'14:35',4];B=['18:30','Job','17:00',I, '16:00', "Duolingo 'French Time'", '22:00', 'Sleep!'];subjects = {'Sunday': ['C.Math', 'Science', 'S.Studies', 'O.Maths', 'HPE'], 'Monday': ['Computer', 'English', 'Nepali', 'C.Maths', 'Science'], 'Tuesday': ['English', 'Social', 'O.Maths', 'Computer', 'HPE'], 'Wednesday': ['C.Maths', 'Nepali', 'Science', 'Social', 'HPE'], 'Thursday': ['O.Maths', 'Computer', 'Nepali', 'English', 'C.Maths'], 'Friday': ['Science', 'Social', 'English', 'O.maths', 'Computer']}
+G=['10:05',0, '11:05',1,'12:05',2,'13:35',3,'14:35',4];B=['18:30','Job','17:00',I, '22:00', 'Sleep!'];subjects = {'Sunday': ['C.Math', 'Science', 'S.Studies', 'O.Maths', 'HPE'], 'Monday': ['Computer', 'English', 'Nepali', 'C.Maths', 'Science'], 'Tuesday': ['English', 'Social', 'O.Maths', 'Computer', 'HPE'], 'Wednesday': ['C.Maths', 'Nepali', 'Science', 'Social', 'HPE'], 'Thursday': ['O.Maths', 'Computer', 'Nepali', 'English', 'C.Maths'], 'Friday': ['Science', 'Social', 'English', 'O.maths', 'Computer']}
 OOSD=E.datetime.now()
 day = OOSD.strftime("%A")
 if day != "Saturday":
--- covidon.py
+++ covidon.py
@@ -1,13 +1,21 @@
-import requests, json, time, os
+import requests, json, time, os, datetime
 i = 0
 def covid_return():
     global i
     try:
+      timer = datetime.datetime.now().strftime("%Y-%m-%d")
       URL = 'https://covid19.mohp.gov.np/covid/api/confirmedcases'
       text = requests.get(URL).text
       data = json.loads(text)['nepal']
-      new = data['today_newcase']
       date = data['date']
+      for i in range(5):
+        if timer == date:
+              break
+        else:
+              time.sleep(420)
+        text = requests.get(URL).text
+        date = json.loads(text)['nepal']['date']
+      new = data['today_newcase']
       recovered = data['today_recovered']
       deaths = data['today_death']
       t_cases = int(new) - int(recovered)
@@ -19,11 +27,10 @@ def covid_return():
       t = b+str(new)+" cases \n"+b+str(recovered)+" recovered \n"+b+ str(deaths)+" deaths \n" +b+str(t_cases)+" total"
       os.system(f'notify-send -i "/home/shal/Documents/code/Privion/c.png" --urgency=critical "{b+date+"    "+a}" "{t}"')
       os.system('play ./co.mp3')
-    except:
-        i += 1
-        os.system(f'notify-send -i "/home/shal/Documents/code/Privion/avatar.jpg" --urgency=critical "No Internet Connection" "I will try in 5 minutes for 5 times"')
-        time.sleep(300)
-        if i < 5:
+    except ConnectionError:
+        for i in range(5):
+            os.system(f'notify-send -i "/home/shal/Documents/code/Privion/avatar.jpg" --urgency=critical "No Internet Connection" "I will try in 5 minutes for 5 times"')
+            time.sleep(300)
             covid_return()
-        else:
-            pass
+if __name__ == "__main__":
+    covid_return()
